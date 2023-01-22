2d6274a4b63d4dd9a93270643028b39b12cc7116
Christina <christinaraganit@gmail.com>
2023-01-21 16:22:36 -0800

    home page mobile boilerplate done

---
+++ frontend/src/routes/home/+page.svelte
@@ -0,0 +1,77 @@
+<div class="home">
+  <div class="home-img">
+    <img
+      src="nommers_logo.svg"
+      alt="Nommers logo"
+    />
+  </div>
+
+  <div class="home-title">
+    <h2>Welcome to Nommers!</h2>
+    <p class="body-text">Say yes to more food and less stress with Nommers.</p>
+
+    <div class="home-actions">
+      <a
+        class="body-title primary-action"
+        href="/create-party">Create party</a
+      >
+      <a class="body-title secondary-action">Join party</a>
+      <a class="body-title secondary-action">Change username</a>
+    </div>
+  </div>
+
+  <div class="user-information">
+    <p class="caption-text">Your username</p>
+    <p class="body-title">FatPenguin</p>
+  </div>
+</div>
+
+<style>
+  .home {
+    padding: var(--sp-lg);
+    height: calc(100vh - var(--sp-lg) - var(--sp-lg));
+    max-height: calc(100vh - var(--sp-lg) - var(--sp-lg));
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-between;
+  }
+
+  .home > div {
+    text-align: center;
+  }
+
+  .home-title {
+    width: 75%;
+  }
+
+  .home-title > p {
+    padding-bottom: var(--sp-md);
+  }
+
+  .home-actions {
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    gap: var(--sp-sm);
+  }
+
+  .primary-action,
+  .secondary-action {
+    width: 100%;
+    padding: var(--sp-md) 0;
+    border-radius: var(--sp-2xl);
+    text-align: center;
+    text-decoration: none;
+  }
+
+  .primary-action {
+    background-color: var(--secondary-500);
+    color: white;
+  }
+
+  .secondary-action {
+    color: var(--secondary-500);
+    background-color: var(--secondary-100);
+  }
+</style>
