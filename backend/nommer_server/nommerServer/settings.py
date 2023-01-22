871f62f6f90f13de60752695ef3ef4f49821cdef
Goldentoaste <rayg2375@gmail.com>
2023-01-22 03:18:26 -0800

    stuff

--- backend/nommer_server/nommerServer/settings.py
+++ backend/nommer_server/nommerServer/settings.py
@@ -29,10 +29,11 @@ ALLOWED_HOSTS = ["*"]
 AUTH_USER_MODEL = "user.User"
 
 # Application definition
-
+CORS_ALLOW_ALL_ORIGINS = True
 INSTALLED_APPS = [
     "channels",
     "daphne",
+    'corsheaders',
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -46,6 +47,8 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+'django.middleware.common.CommonMiddleware',
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -53,6 +56,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    
 ]
 
 ROOT_URLCONF = "nommerServer.urls"
--- frontend/src/routes/signup/+page.svelte
+++ frontend/src/routes/signup/+page.svelte
@@ -1,7 +1,39 @@
 
 <script>
+  import { accountId } from "../../account";
+  let username = "";
+  let password= "";
 
-  
+  const signup = ()=>{
+
+    if (username.length == 0 || password.length == 0){
+      return;
+    }
+
+    fetch('http://server3-env.eba-7jgvjkan.us-west-2.elasticbeanstalk.com/signup/',
+    {
+      method: 'POST',
+      headers:{
+        'Accept':'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(
+        {
+          'username':username,
+          'password':password,
+          'ppiurl':""
+        }
+      )
+    }).then((res)=>{
+      return res.json()
+    }).then(
+      (res)=>{
+        $accountId = res["id"]
+        console.log($accountId)
+      }
+    )
+
+  }
 </script>
 
 <div class="login">
@@ -16,17 +48,21 @@
       <input
         type="text"
         placeholder="Username"
+        bind:value={username}
       />
 
       <input
         type="password"
         placeholder="Password"
+        bind:value={password}
       />
     </div>
 
     <div class="login-actions">
+      <!-- svelte-ignore a11y-click-events-have-key-events -->
       <div
         class="body-title primary-action"
+        on:click={signup}
        >Create account</div
       >
       <a
