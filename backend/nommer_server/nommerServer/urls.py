cf947da53dcb4a8112a44b7c9e8f0b7620166a9a
Goldentoaste <rayg2375@gmail.com>
2023-01-21 16:31:16 -0800

    health

--- backend/nommer_server/nommerServer/urls.py
+++ backend/nommer_server/nommerServer/urls.py
@@ -16,6 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from user.views import health_check, signIn, signUp
+
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("health/", health_check),
+    path("signup/", signUp),
+    path("signin/", signIn)
 ]
