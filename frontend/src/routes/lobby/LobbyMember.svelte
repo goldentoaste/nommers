fbc811ad14fb8a00065a9e595585deae5125ce62
Christina <christinaraganit@gmail.com>
2023-01-21 18:42:18 -0800

    lobby mobile

--- frontend/src/routes/lobby/LobbyMember.svelte
+++ frontend/src/routes/lobby/LobbyMember.svelte
@@ -3,12 +3,56 @@
   export let isOwner;
 </script>
 
-<div class="lobby-member">
-  {username}
-  {#if isOwner}
-    <img
-      src="/placeholder.png"
-      alt="Placeholder"
-    />
-  {/if}
+<div
+  class="lobby-member"
+  style="background-color: var({isOwner ? '--secondary-100' : '--black-100'})"
+>
+  <div class="member-info">
+    <span>{username}</span>
+
+    <div class="member-icon">
+      {#if isOwner}
+        <img
+          src="/icons/star.svg"
+          alt="Lobby owner icon"
+        />
+      {/if}
+
+      {#if !isOwner}
+        <img
+          src="/icons/close.svg"
+          alt="Remove member icon"
+        />
+      {/if}
+    </div>
+  </div>
 </div>
+
+<style>
+  .lobby-member {
+    margin: var(--sp-xs);
+    display: inline-block;
+    width: fit-content;
+    padding: var(--sp-xs) var(--sp-md);
+    border-radius: var(--sp-xl);
+  }
+
+  .member-info {
+    display: inline-flex;
+    align-items: center;
+    flex-direction: row;
+    gap: var(--sp-2xs);
+  }
+
+  .member-icon {
+    display: flex;
+    align-items: center;
+    height: var(--sp-md);
+    width: auto;
+    aspect-ratio: 1/1;
+  }
+
+  .member-icon > img {
+    height: 100%;
+  }
+</style>
